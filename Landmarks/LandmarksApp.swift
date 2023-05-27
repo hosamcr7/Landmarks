854b0f962b8ed33b82a12059da37333791b43261
hosam al-deen abufasha <hosamabufashacr7@hotmail.com>
2023-05-27 17:06:49 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by hosam abufasha on 27/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
